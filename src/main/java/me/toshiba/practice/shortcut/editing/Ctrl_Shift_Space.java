6d5982c7e402e59510a12362285c5bce464c41a6
tohsiba <myostmail@163.com>
2020-04-27 18:14:45 +0800

    增加代码注释demo

--- src/main/java/me/toshiba/practice/shortcut/editing/Ctrl_Shift_Space.java
+++ src/main/java/me/toshiba/practice/shortcut/editing/Ctrl_Shift_Space.java
@@ -5,13 +5,12 @@ package me.toshiba.practice.shortcut.editing;
  *
  * @description 根据上下文自动补全代码
  * @shortcut Ctrl + Shift + 空格
- * @notes
  */
 public class Ctrl_Shift_Space {
 
     private void showUsage() {
          /**
-         取消第 18 行注释 （注释快捷键参见{@link Ctrl_Virgule})
+         取消第 17 行注释 （注释快捷键参见{@link Ctrl_Virgule})
          将光标定位到‘new’关键字后
          先敲入一个空格，之后点击 Ctrl+空格
          */
--- src/main/java/me/toshiba/practice/shortcut/editing/Ctrl_Virgule.java
+++ src/main/java/me/toshiba/practice/shortcut/editing/Ctrl_Virgule.java
@@ -3,6 +3,18 @@ package me.toshiba.practice.shortcut.editing;
 /**
  * 单行代码注释
  *
+ * @description 注释单行代码，或恢复已注释的单行代码
+ * @shortcut Ctrl + /
  */
 public class Ctrl_Virgule {
+
+    private void showUsage() {
+        /**
+         * 将光标定位到第 17 行
+         * 点击 Ctrl + / 对代码进行注释（注意操作后光标位置会下移）
+         * 再次点击 Ctrl + / 恢复被注释的代码
+         */
+        System.out.println("封印我吧！");
+//        System.out.println("释放我吧！");
+    }
 }
